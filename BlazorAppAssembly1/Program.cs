5a5e1fcd494addc8150c6a292075a25158598e81
Augusto Paz <augustoxx2015@gmail.com>
2023-08-10 13:49:25 -0300

    New Models and Services files, products in pages

--- BlazorAppAssembly1/Program.cs
+++ BlazorAppAssembly1/Program.cs
@@ -1,12 +1,16 @@
 using BlazorAppAssembly1;
+using blazorappdemo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 var apiURL = builder.Configuration.GetValue<string>("apiURL");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiURL) });
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 await builder.Build().RunAsync();
